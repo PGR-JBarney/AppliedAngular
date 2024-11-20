6a71961e1593b054dac98f51780a6e36869ab690
Jason Barney <Jason_M_Barney@progressive.com>
2024-11-20 12:39:02 -0500

    Create feature and routing

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -23,6 +23,11 @@ export const routes: Routes = [
       import('./features/atm/atm.routes').then((r) => r.ATM_ROUTES),
   },
   {
+    path: 'counter',
+    loadChildren: () =>
+      import('./counter/counter.routes').then((r) => r.COUNTER_ROUTES),
+  },
+  {
     path: '**',
     redirectTo: 'home',
   },
